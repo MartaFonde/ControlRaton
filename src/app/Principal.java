d249438d3f7660e2bf55f8b110971dc3026fe28a
Marta Fondevila <martafondevila8@gmail.com>
2020-05-11 13:01:34 +0200

    Prueba 2

--- src/app/Principal.java
+++ src/app/Principal.java
@@ -19,6 +19,7 @@ public class Principal extends JFrame {
         titulo = getTitle();
         setLayout(null);
         setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
 
         lbl = new JLabel("Teclas");
         lbl.setSize(150,60);
@@ -30,6 +31,7 @@ public class Principal extends JFrame {
         btnIzq.setLocation(20,30);
         btnIzq.setBackground(Color.GRAY);
         btnIzq.addMouseListener(new MouseHandler());
+        btnIzq.addMouseMotionListener(new MouseHandler());
         add(btnIzq);
 
         btnDer = new JButton("Der");
@@ -37,25 +39,31 @@ public class Principal extends JFrame {
         btnDer.setLocation(80,30);
         btnDer.setBackground(Color.gray);
         btnDer.addMouseListener(new MouseHandler());
+        btnDer.addMouseMotionListener(new MouseHandler());
         add(btnDer);
 
-        this.getContentPane().addMouseListener(new MouseHandler());
+        addMouseListener(new MouseHandler());
+        addMouseMotionListener(new MouseHandler());
         addKeyListener(new KeyHandler());
         addWindowListener(new WindowHandler());
         s = new Secundario(this);
     }
 
     class MouseHandler extends MouseAdapter{
-        public void mouseEntered(java.awt.event.MouseEvent e) {
-            if(e.getXOnScreen() < 800 && e.getYOnScreen() < 600){
-                setTitle(titulo+" - X:"+e.getXOnScreen()+", Y:"+e.getYOnScreen());
+        @Override
+        public void mouseMoved(java.awt.event.MouseEvent e) {
+            if(e.getSource() == btnIzq) {
+                setTitle(titulo+" - (X:"+(btnIzq.getX()+ e.getX())+", Y:"+(btnIzq.getY() + e.getY() + 25)+")");
+ 
+            }else if(e.getSource() == btnDer) {
+                setTitle(titulo+" - (X:"+(btnDer.getX() + e.getX())+", Y:"+(btnDer.getY() + e.getY() + 25)+")");
+ 
+            } else {
+                setTitle(titulo+" - (X:"+e.getX()+", Y:"+e.getY() + ")");
             }
         }
 
-        public void mouseExited(java.awt.event.MouseEvent e) {
-            setTitle(titulo);
-        }
-
+        @Override
         public void mousePressed(java.awt.event.MouseEvent e) {
             if(e.getButton() == e.BUTTON1){
                 btnIzq.setBackground(colorIzq);
@@ -63,8 +71,10 @@ public class Principal extends JFrame {
             if(e.getButton() == e.BUTTON3){
                 btnDer.setBackground(colorDer);
             }
+            requestFocus();
         }
 
+        @Override
         public void mouseReleased(java.awt.event.MouseEvent e) {
             if(e.getButton() == e.BUTTON1){
                 btnIzq.setBackground(Color.GRAY);
@@ -72,6 +82,7 @@ public class Principal extends JFrame {
             if(e.getButton() == e.BUTTON3){
                 btnDer.setBackground(Color.GRAY);
             }
+            requestFocus();
         }
     }
 
@@ -79,8 +90,8 @@ public class Principal extends JFrame {
         String texto=lbl.getText();
         
         public void keyPressed(java.awt.event.KeyEvent e) {
-            lbl.setText("<html><body>Unicode: "+e.getKeyChar()+
-            "<br>Código teclado: "+e.getKeyCode()+"</body></html>");
+            lbl.setText("<html><body>Código Unicode: " + (int) e.getKeyChar() +
+            "<br>Código teclado: " + e.getKeyCode() + "</body></html>");
             
             if(e.isControlDown() && e.getKeyCode() == 'C'){
                 s.pack();
